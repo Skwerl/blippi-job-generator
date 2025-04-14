f9a93d98dfe4ef0a404b13d4e2ecd1705ff4afbf
Kevin Cogill <skwerl@gmail.com>
2025-04-14 10:04:35 -0400

    Results images

--- src/Components/ResultCharacters.jsx
+++ src/Components/ResultCharacters.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import * as Motion from "motion/react-client";
 
 const ResultsCharacter = (props) => {
-  const { side, size, children } = props;
+  const { side, size, style, children } = props;
   const rotation = (side === "right") ? [0, -2, 0] : [0, 2, 0];
-  return <div className={`results-character ${side}`} style={{ height: `${size}%` }}>
+  return <div className={`results-character ${side}`} style={{ ...style, height: `${size}%` }}>
     <Motion.div
       animate={{
         rotate: rotation
@@ -26,6 +26,78 @@ export default function ResultCharacters(props) {
 
   return <React.Fragment>
 
+    {(result === "Art Teacher") && <React.Fragment>
+      <ResultsCharacter side="left" size={100}>
+        <img src="/images/result-artist-blippi.png" />
+      </ResultsCharacter>
+      <ResultsCharacter side="right" size={94}>
+        <img src="/images/result-artist-meekah.png" />
+      </ResultsCharacter>
+    </React.Fragment>}
+
+    {(result === "Astronaut") && <React.Fragment>
+      <ResultsCharacter side="left" size={94}>
+        <img src="/images/result-astronaut-meekah.png" />
+      </ResultsCharacter>
+      <ResultsCharacter side="right" size={100}>
+        <img src="/images/result-astronaut-blippi.png" />
+      </ResultsCharacter>
+    </React.Fragment>}
+
+    {(result === "Construction Worker") && <React.Fragment>
+      <ResultsCharacter side="left" size={94} style={{ left: "-6%" }}>
+        <img src="/images/result-construction-meekah.png" />
+      </ResultsCharacter>
+      <ResultsCharacter side="right" size={100}>
+        <img src="/images/result-construction-blippi.png" />
+      </ResultsCharacter>
+    </React.Fragment>}
+
+    {(result === "Dancer") && <React.Fragment>
+      <ResultsCharacter side="left" size={112} style={{ left: "-16%" }}>
+        <img src="/images/result-dancer-meekah.png" />
+      </ResultsCharacter>
+      <ResultsCharacter side="right" size={100}>
+        <img src="/images/result-dancer-blippi.png" />
+      </ResultsCharacter>
+    </React.Fragment>}
+
+    {(result === "Doctor") && <React.Fragment>
+      <ResultsCharacter side="left" size={94}>
+        <img src="/images/result-doctor-meekah.png" />
+      </ResultsCharacter>
+      <ResultsCharacter side="right" size={100} style={{ right: "-9%" }}>
+        <img src="/images/result-doctor-blippi.png" />
+      </ResultsCharacter>
+    </React.Fragment>}
+
+    {(result === "Farmer") && <React.Fragment>
+      <ResultsCharacter side="left" size={94} style={{ left: "-9%" }}>
+        <img src="/images/result-farmer-meekah.png" />
+      </ResultsCharacter>
+      <ResultsCharacter side="right" size={100} style={{ right: "-9%" }}>
+        <img src="/images/result-farmer-blippi.png" />
+      </ResultsCharacter>
+    </React.Fragment>}
+
+    {(result === "Firefighter") && <React.Fragment>
+      <ResultsCharacter side="left" size={100}>
+        <img src="/images/result-firefighter-blippi.png" />
+      </ResultsCharacter>
+      <ResultsCharacter side="right" size={94} style={{ right: "-11%" }}>
+        <img src="/images/result-firefighter-meekah.png" />
+      </ResultsCharacter>
+    </React.Fragment>}
+
+    {(result === "Grocery Worker") && <React.Fragment>
+      <ResultsCharacter side="left" size={100}>
+        <img src="/images/result-grocer-blippi.png" />
+      </ResultsCharacter>
+      <ResultsCharacter side="right" size={94} style={{ right: "-11%" }}>
+        <img src="/images/result-grocer-meekah.png" />
+      </ResultsCharacter>
+    </React.Fragment>}
+
     {(result === "Monster Truck Driver") && <React.Fragment>
       <ResultsCharacter side="left" size={100}>
         <img src="/images/result-monstertruck-blippi.png" />
@@ -35,6 +107,15 @@ export default function ResultCharacters(props) {
       </ResultsCharacter>
     </React.Fragment>}
 
-  </React.Fragment>
+    {(result === "Pizza Chef") && <React.Fragment>
+      <ResultsCharacter side="left" size={94}>
+        <img src="/images/result-pizza-meekah.png" />
+      </ResultsCharacter>
+      <ResultsCharacter side="right" size={100} style={{ right: "-7%" }}>
+        <img src="/images/result-pizza-blippi.png" />
+      </ResultsCharacter>
+    </React.Fragment>}
+
+  </React.Fragment >
 
 }
