10dbe0f289d3b2ee279aaaf592e033e7f76c884f
Kevin Cogill <skwerl@gmail.com>
2025-04-13 20:45:40 -0400

    Results character images

--- src/Pages/Quiz.jsx
+++ src/Pages/Quiz.jsx
@@ -38,6 +38,15 @@ export default function Quiz() {
               </Stack>
             </div>
 
+            {(jobResult === "Monster Truck Driver") && <React.Fragment>
+              <ResultsCharacter side="left" size={100}>
+                <img src="/images/result-monstertruck-blippi.png" />
+              </ResultsCharacter>
+              <ResultsCharacter side="right" size={94}>
+                <img src="/images/result-monstertruck-meekah.png" />
+              </ResultsCharacter>
+            </React.Fragment>}
+
           </React.Fragment>
           : <React.Fragment>
             <div className="prompt">
@@ -70,4 +79,23 @@ export default function Quiz() {
     </React.Fragment>
   </Main>
 
+}
+
+const ResultsCharacter = (props) => {
+  const { side, size, children } = props;
+  const rotation = (side === "right") ? [0, -2, 0] : [0, 2, 0];
+  return <div className={`results-character ${side}`} style={{ height: `${size}%` }}>
+    <Motion.div
+      animate={{
+        rotate: rotation
+      }}
+      transition={{
+        duration: 2,
+        ease: "easeInOut",
+        repeat: Infinity,
+        repeatDelay: 1
+      }}>
+      {children}
+    </Motion.div>
+  </div>
 }
