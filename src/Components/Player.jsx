e9b9d58b95c25240c01692824fb307f0bb441831
Kevin Cogill <skwerl@gmail.com>
2025-04-17 13:02:05 -0400

    Linkfire embed and replay

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import * as Motion from "motion/react-client";
 import { Howl } from 'howler';
 
+import './Player.scss';
+
 export default function Player(props) {
 
   const { snippet } = props;
+  const [embedDisplay, setEmbedDisplay] = useState(false);
+  const [replayEnable, setReplayEnable] = useState(false);
 
   const AudioPlayer = new Howl({
     src: [snippet],
     html5: true,
     volume: 1,
     onend: function () {
-      console.log('Finished!');
+      setReplayEnable(true);
     }
   });
 
   const playSnippet = () => {
     AudioPlayer.play();
+    setTimeout(() => {
+      setEmbedDisplay(true);
+    }, 10000);
+  }
+
+  const handleReplay = () => {
+    AudioPlayer.play();
   }
 
   return <React.Fragment>
+
+    {embedDisplay && <div className="modal-embed">
+      <div className="iframe-wrapper">
+        <iframe
+          src="https://blippi.lnk.to/WeExploreJG/widget"
+          width="400"
+          height="500"
+          frameBorder="0"
+          allowTransparency="true"
+        ></iframe>
+        {replayEnable && <div className="button">
+          <img src="/images/button-replay.png" onClick={handleReplay} />
+        </div>}
+      </div>
+    </div>}
 
     <div className="button">
       <Box>
