294ad7dbca8acfb94cc25dfdca1e9ebbc0a8536e
Kevin Cogill <skwerl@gmail.com>
2025-04-17 12:27:47 -0400

    Shush Redux stuff

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { Store } from './Redux/Store'
+// import { Provider } from 'react-redux';
+// import { Store } from './Redux/Store'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <Provider store={Store}>
-    <App />
-  </Provider>
+  // <Provider store={Store}>
+  <App />
+  // </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
