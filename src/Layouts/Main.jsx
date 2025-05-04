13791e98828f008432d31e84dd53a2f991a26f12
Kevin Cogill <skwerl@gmail.com>
2025-05-04 19:27:41 -0400

    Added share icon

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import Box from '@mui/material/Box';
 import AspectRatio from '@mui/joy/AspectRatio';
+import * as Motion from "motion/react-client";
 
 import './Main.scss';
 
@@ -11,6 +12,10 @@ export default function Main(props) {
     useEffect(() => {
         document.title = (!!pagetitle ? pagetitle : "Blippi Job Generator");
     }, [pagetitle]);
+
+    const openShare = () => {
+        console.log("open share");
+    }
 
     return <Box className="master_wrapper">
 
@@ -23,6 +28,9 @@ export default function Main(props) {
                     {children}
                 </React.Fragment>
             </Box>
+            <Motion.div className="share_button" whileHover={{ scale: 1.2 }} onClick={openShare}>
+                <img src="/images/icon-share.png" />
+            </Motion.div>
         </AspectRatio>
 
     </Box >
