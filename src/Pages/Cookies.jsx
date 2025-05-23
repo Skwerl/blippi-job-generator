db51ce80a4e931c4c0512ed5226c8a7ee2732120
Kevin Cogill <skwerl@gmail.com>
2025-05-23 05:57:43 -0400

    Cookie policy

--- src/Pages/Cookies.jsx
+++ src/Pages/Cookies.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Basic from '../Layouts/Basic';
+import { LAUNCH_DATE } from "../Logic";
 
 export default function Cookies() {
 
@@ -8,15 +10,108 @@ export default function Cookies() {
 
       <h1>Cookie Policy</h1>
 
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque facilisis sodales tortor, eu fringilla elit molestie eu. Vivamus eget massa et orci imperdiet porttitor a non erat. Cras sed est vel eros tincidunt vestibulum porttitor eget felis. Proin aliquet vitae erat vehicula congue. Maecenas maximus, mi quis tincidunt sollicitudin, sapien quam ultricies ligula, nec facilisis arcu tortor mollis nibh. Quisque laoreet enim nec felis interdum consectetur. Donec blandit, justo non placerat dictum, dolor turpis ullamcorper leo, facilisis maximus justo odio nec mi. Donec sit amet ante tristique, vehicula lectus ac, venenatis odio. Morbi convallis pretium pretium. Curabitur ut commodo ligula. Morbi semper nibh ante, porta aliquet lectus iaculis nec.</p>
+      <p>Last updated {LAUNCH_DATE}.</p>
 
-      <p>Cras nisi metus, gravida rhoncus purus ut, posuere fringilla eros. Phasellus est urna, pulvinar in urna vitae, maximus semper turpis. Ut ipsum urna, sodales at tristique eu, volutpat et nibh. Fusce in suscipit sem. Suspendisse gravida tincidunt turpis non aliquam. Aenean leo metus, consectetur facilisis commodo eget, consectetur et velit. Mauris pharetra augue massa, eget euismod enim lacinia in. Aliquam vulputate dolor a justo commodo pharetra. Nulla id posuere ex. Donec semper molestie augue at efficitur. Aliquam id ipsum turpis. Suspendisse potenti. Sed vitae suscipit neque. Nulla ac volutpat ipsum. Proin sit amet pretium metus.</p>
+      <p><strong>Introduction</strong></p>
 
-      <p>Maecenas viverra dui sed ligula commodo dapibus. In lacinia id risus ut tincidunt. Sed euismod leo id lectus blandit, eget rhoncus lacus vehicula. Nullam elit nibh, tempus ut sem non, condimentum varius dui. Nulla eu metus ipsum. Cras ultricies ex eu vehicula scelerisque. Donec ac nisl nibh. Proin id ante in mauris blandit consequat sed eu dolor. Mauris ultricies tortor nec sodales tempor.</p>
+      <p>This Cookie Policy is issued on behalf of Moonbug Entertainment Limited, company number 11197631,  data protection registration number ZA457059, with registered office at 3rd Floor, Labs Upper Lock, 3-6 Water Lane, London NW1 8JZ, United Kingdom and, (<strong>“Moonbug”</strong>, <strong>“us”</strong>, <strong>“we”</strong> or <strong>“our”</strong>). We are responsible for the micro-site site located at <a href="https://www.blippijobgenerator.com">www.BlippiJobGenerator.com</a> (the <strong>“Microsite”</strong>) and this Cookie Policy applies to your use of the Microsite.</p>
 
-      <p>Aliquam gravida, lacus imperdiet finibus ultricies, magna ante vulputate dui, nec lobortis odio libero et lectus. Aliquam at nisi nisi. Mauris tincidunt at odio at interdum. Suspendisse molestie tincidunt mi eget molestie. Vestibulum vehicula ut diam et cursus. Vestibulum non maximus velit. Quisque congue elementum lorem rhoncus facilisis. Curabitur et dolor nec est hendrerit scelerisque. Nullam vel ex nisl. Mauris accumsan, enim et eleifend efficitur, mauris sem dapibus massa, eu imperdiet velit neque sit amet arcu. Nunc a finibus massa. Aliquam feugiat euismod felis, eget fringilla ex semper pulvinar. Praesent ac augue leo. Integer rutrum mauris ut sapien ultrices, eu lacinia magna imperdiet.</p>
+      <p>We refer to everyone who accesses or uses the Microsite as a <strong>“User”</strong>. For the purpose of these Terms of Use, <strong>“you”</strong> and <strong>“your”</strong> means you as the User of the Microsite.</p>
 
-      <p>Donec ipsum magna, auctor a nibh vitae, varius semper mi. Vestibulum ac ante non mauris lacinia tempus sed vitae arcu. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce hendrerit sit amet urna vel consectetur. Nulla nisi nulla, congue ullamcorper turpis id, laoreet vehicula felis. Integer sed vulputate felis. Vivamus a magna ut tortor facilisis egestas. Mauris venenatis tempus nibh, vitae scelerisque ex efficitur vitae. Quisque ut cursus nunc, sed vehicula sem. Quisque vitae ex eros. Fusce non turpis at magna gravida faucibus ut at nisi. Phasellus sapien ante, placerat a dolor eget, molestie feugiat risus. Sed vitae velit in est faucibus egestas eu sit amet augue. Mauris mattis nisi ut consequat lobortis.</p>
+      <p>This Cookie Policy explains how we use “cookies” and similar technologies on the Microsite. It is designed to assist you in making informed decisions when interacting with our content, so please take a moment to read and understand this Cookie Policy.</p>
+
+      <p>For further information about how we process your personal data when accessing the Microsite please visit our <Link to="/terms">Terms of Use</Link> and <Link to="/privacy">Privavcy Policy</Link>.</p>
+
+      <p><strong>What are cookies or similar technologies?</strong></p>
+
+      <p>Our Microsite uses cookies to distinguish you from other users. This helps us to provide you with a good experience when you browse our content and also allows us to improve our content.</p>
+
+      <p>A cookie is a small text file (letters and numbers) or a piece of code, which often includes a unique identifier. When you visit a website, the website will request to store this text file on your device in order to remember information about you, such as your language preference. Other information gathered through cookies may include the date and time of visits and how you are using a particular website.</p>
+
+      <p>We may also use similar technologies to cookies known as “web beacons”, “pixels”, “tags” or SDKs in our Microsite. The above technologies do a similar job to cookies, allowing operators to count views and understand how users interact with and respond to certain content on a webpage.</p>
+
+      <p>For the purposes of the remaining sections of this Cookie Policy, we refer to all cookies and similar technologies using the above features as “cookies”.</p>
+
+      <p>We may use “first party cookies” and “third party cookies” within our Microsite. First party cookies are cookies placed by us to collect information about you. Third party cookies are cookies placed by third-party operators on our Microsite. Information about you collected by those third party cookies will be shared with the relevant third-party. Please refer to the relevant third-party operator’s privacy notice below for more details about the information they collect and how they use it.</p>
+
+      <p>Please note that our Microsite may also link to external third-party websites which are not operated by us. Where you follow links from our Microsite to the website or platform of a third-party (even if you do not actually leave our Microsite), that website or platform may place different cookies on your device. You should check the relevant privacy notice and/or cookie notice for more information about how that-third party uses cookies.</p>
+
+      <p>You should be aware that applications you use to access our Microsite, such as your website browser, may also place cookies on your device when visiting our Microsite. We do not have control over these third-party cookies, so you will need to manage these cookies in the settings of the relevant applications.</p>
+
+      <p>For more details on cookies and similar technologies, please visit www.allaboutcookies.org.</p>
+
+      <p><strong>What are the different types of cookies?</strong></p>
+
+      <p>
+        <ol>
+          <li>Strictly necessary cookies. These are cookies that are required for the operation of our Microsite. They include, for example, cookies required for security reasons or fixing bugs.</li>
+          <li>Analytical/performance cookies. They allow us to recognise and count the number of visitors and to see how visitors move around our Microsite when they are using it. This helps us to improve the way our Microsite work, for example, by ensuring that users are finding what they are looking for easily.</li>
+          <li>Functionality cookies. These are used to recognise you when you return to our Microsite, and may be used to remember your preferences and provide enhanced, more personalised features.</li>
+          <li>Targeting cookies. Targeting cookies (also known as advertising cookies) help advertisers and media publishers to deliver adverts beyond the original website you have visited, for example when you browse a shopping website and then visit a news websites. This is known as ‘retargeting’. This technique helps to ensure that the ads you receive are more relevant to you and your interests. These cookies  help to measure the effectiveness of an ad (for example, by reporting whether you have clicked on it) and can also limit the number of times that you see an ad (for example, if you see it twice but don’t click on it, you may not see it again). These cookies are usually placed by advertising networks that advertisers work with to deliver ads. They remember that you have visited a website and this information is shared with other ad networks to show you relevant ads.</li>
+          <li>Social media cookies.  These cookies are used to personalise your interaction with social media, such as Facebook, Instagram, and YouTube. Such cookies recognise users of those social media sites when you view social media content on the website. They also allow you to quickly share content across social media, through the use of simple “sharing” buttons.</li>
+          <li>Security and website integrity cookies. These cookies help keep your data and websites safe and secure.</li>
+        </ol>
+      </p>
+
+      <p><strong>How long do cookies last?</strong></p>
+
+      <p>All cookies have expiration dates that determine how long they stay in your browser:</p>
+
+      <p>Session Cookies – these are temporary cookies that are placed on your device during your browsing session and then expire (and are automatically erased) whenever you close your browser.</p>
+
+      <p>Persistent Cookies – these are designed to last for a pre-defined period of time, which varies depending on the nature of the cookie. Persistent cookies stay in your browser until they reach their individual expiry date, or until you manually delete them.</p>
+
+      <p><strong>What Cookies do we use on the Microsite?</strong></p>
+
+      <p>You can find more information below about the individual cookies we use on the Microsite, their duration and the purposes for which we use them and the third-parties who we share data with.</p>
+
+      <p><strong>Strictly Necessary Cookies</strong></p>
+
+      <p>The Wix platform, which we use to operate this Microsite, has set strictly necessary cookies on the Microsite for the following purposes: fraud detection of calls; security; session identification; performance for rendering; attack detection; system effectiveness measurement, tracking session errors and issues (resilience); site experiments; API protection. These cookies will be automatically deployed onto your computer when you use the Microsite. You can review the Wix’s Cookies Policy <a href="https://support.wix.com/en/article/cookies-and-your-wix-site">here</a>.</p>
+
+      <p>By continuing to browse this Microsite, you are agreeing to the use of these strictly necessary cookies.</p>
+
+      <p><strong>Non-Essential Cookies</strong></p>
+
+      <p>We use Google Analytics, which is a web analytics tool that helps us understand how users engage with the Microsite. Like many services, Google Analytics uses first-party cookies to track user interactions, as in our case, where they are used to collect information about how users use our site. This information is used to compile reports and to help us improve our service. The reports disclose website trends without identifying individual visitors. For more information on Google Tag Manager, see <a href="https://support.google.com/tagmanager/answer/6102821?">here</a>. For more information on Google Analytics, see <a href="https://policies.google.com/technologies/partner-sites">here</a>⁠.</p>
+
+      <p>You may accept or decline these Google Analytics cookies depending on whether you are happy or not for them to be deployed onto your device.</p>
+
+      <p><strong>Is consent needed to use cookies?</strong></p>
+
+      <p><strong>Essential Cookies</strong></p>
+
+      <p>Please note that we do not need your consent to store and access essential cookies on your device, although you can still block or delete them (see below).</p>
+
+      <p>Non-essential Cookies</p>
+
+      <p>We may only store and access non-essential cookies on your device with your permission. You are not obliged to give consent to our use of non-essential cookies. If you do give your consent and then change your mind, you can block or delete them (see below).</p>
+
+      <p><strong>How do I refuse, block or delete cookies?</strong></p>
+
+      <p>There are various ways that you can manage your cookie preferences, but please be aware that in order to use some parts of our Microsite, you will need to allow certain essential or functional cookies. If you block or subsequently delete those cookies, some aspects of our Microsite may not work properly and you may not be able to access all or part of our content.</p>
+
+      <p>Your options for managing your cookie preferences are set out below:</p>
+
+      <p><strong>Through the relevant Preference Centre on our Microsite</strong></p>
+
+      <p>When you access our Microsite, we will ask you to provide consent for our use of non-essential cookies. We may repeat this request on subsequent visits to our Microsite, for example if you delete cookies from your browser or we need to request new consents from you.</p>
+
+      <p>When we seek your consent, you will either be able to provide consent for all non-essential cookies that we would like to use, or you can tailor your cookie preferences (provide consent for some but not all non-essential cookies) using our Preference Centre. You can also change your preferences and/or withdraw consent at any time by visiting the relevant Preference Centre.</p>
+
+      <p><strong>Using your browser settings or other tools</strong></p>
+
+      <p>You may block some cookies within the Microsite by activating the setting on your browser that allows you to refuse the setting of all or some cookies. Alternatively, you can visit <a href="https://www.allaboutcookies.org">www.allaboutcookies.org</a> which provides general information about cookies and how you can manage cookies on your device. Please note that if you use your browser settings to block all cookies (including essential cookies) you may not be able to access all or parts of this Microsite.</p>
+
+      <p>If you do not want to be tracked by Google Analytics on our Microsite, you can install the Google Analytics opt-out browser add-on (but this will only work on certain browsers). For further information, please consult: <a href="https://tools.google.com/dlpage/gaoptout">https://tools.google.com/dlpage/gaoptout</a>.</p>
+
+      <p><strong>Will this Cookie Policy change?</strong></p>
+
+      <p>We may update our Cookie Policy from time to time. This might be for a number of reasons, such as to reflect a change in the law or to accommodate a change in our business practices and the way we use cookies. We recommend that you check here periodically for any changes to our Cookie Policy.</p>
+
+      <p><strong>Contact us</strong></p>
+
+      <p>If you have any queries regarding this Cookie Policy, please contact us using <a href="mailto:privacy@moonbug.com">privacy@moonbug.com</a>.</p>
 
     </React.Fragment>
   </Basic>
